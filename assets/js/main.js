f5a7ae8c0bd324514303f27f9f4174c2742cd375
Vinícius <vinicius.nevesco@gmail.com>
2022-02-08 16:40:43 -0300

    version 2.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,20 @@
 "use strict";
 
+// LIKE POST TUTURIAL
+let heart = document.querySelector('.ri-heart-line');
+
+heart.addEventListener('click', () => {
+    let classe = heart.classList.contains('ri-heart-line');
+    if(classe === true) {
+        heart.classList.remove('ri-heart-line', 'ri-2x');
+        heart.classList.add('ri-heart-fill', 'ri-2x');           
+    } else {
+        heart.classList.remove('ri-heart-fill', 'ri-2x');
+        heart.classList.add('ri-heart-line', 'ri-2x');
+    };
+})
+
+
 // MODAL
 let btnCreate = document.querySelector('#create');
 let modal = document.querySelector('.modal-container');
@@ -27,8 +42,8 @@ let titleInput = document.querySelector('#title');
 let contentInput = document.querySelector('#content');
 let warn = document.querySelector('#warn');
 
-const criarArtigo = (titulo, paragrafo) => {
-// função data atual
+const criarArtigo = (titulo, paragrafo, like = false) => {
+    // função data atual
     const dateNow = () => {
         const data = new Date()
         const dia = data.getDate();
@@ -51,83 +66,16 @@ const criarArtigo = (titulo, paragrafo) => {
         };
     };
 
-    // selecionando o main
-    const main = document.querySelector('main');
+    // adicionar a localstorage
+    const dataAtual = dateNow();
+    let post = JSON.parse(localStorage.getItem('post')) || [];
+    post.push({dataAtual, titulo, paragrafo, like});
 
-    // criando artigo
-    const artigo = document.createElement('div');
-        artigo.classList.add('artigo');
-    main.appendChild(artigo);
-
-    // header
-    const headerArtigo = document.createElement('div');;
-    headerArtigo.classList.add('headerArtigo');
-
-        // div data
-        const data = document.createElement('div');
-            
-        // data
-        const p = document.createElement('p');
-        p.innerText = dateNow();
-        
-        data.appendChild(p);
-
-        // div botões
-        const buttons = document.createElement('div');
-
-            const heart = document.createElement('i');
-            heart.classList.add('ri-heart-line', 'ri-2x', 'heart');
-
-            const bin = document.createElement('i');
-            bin.classList.add('ri-delete-bin-line', 'ri-2x', 'bin');
-        
-        // add botões
-        buttons.appendChild(heart);
-        buttons.appendChild(bin);
-
-    // adicionar no header
-    headerArtigo.appendChild(data);
-    headerArtigo.appendChild(buttons);
-
-    // conteúdo
-    const contentArticle = document.createElement('div');
-    contentArticle.classList.add('content');
-
-        // título
-        const h1 = document.createElement('h1');
-        h1.innerText = titulo;
-
-        // parágrafo
-        const paragraph = document.createElement('p');
-        paragraph.innerText = paragrafo;
-
-    // adicionar ao conteúdo
-    contentArticle.appendChild(h1);
-    contentArticle.appendChild(paragraph);
-
-    // adicionar ao artigo
-    artigo.appendChild(headerArtigo);
-    artigo.appendChild(contentArticle);
-
-    // evento favoritar
-    heart.addEventListener('click', () => {
-        let classe = heart.classList.contains('ri-heart-line');
-        if(classe === true) {
-            heart.classList.remove('ri-heart-line', 'ri-2x');
-            heart.classList.add('ri-heart-fill', 'ri-2x');
-        } else {
-            heart.classList.remove('ri-heart-fill', 'ri-2x');
-            heart.classList.add('ri-heart-line', 'ri-2x');
-        };
-    });
-    
-    // evento excluir
-    bin.addEventListener('click', () => {
-        main.removeChild(artigo);
-    });
+    localStorage.setItem('post', JSON.stringify(post));
+    window.location.reload();
 };
 
-// evento criar artigo
+// CRIAR ARTIGO
 btnSubmit.addEventListener('click', (e) => {
     e.preventDefault()
 
@@ -144,11 +92,122 @@ btnSubmit.addEventListener('click', (e) => {
     } else {
         criarArtigo(titleInput.value, contentInput.value);
         modal.style.display = 'none';
-        
-        let post = JSON.parse(localStorage.getItem('artigo')) || [];
-        post.push({dataAtual, titulo, paragrafo});
-    
-        localStorage.setItem('artigo', JSON.stringify(post));
     };
 });
 
+// CRIAR POST LOCALSTORAGE
+const gerarPost = (date, title, content, like) => {
+    // selecionando o main
+    const main = document.querySelector('main');
+
+    // criando artigo
+    const artigo = document.createElement('div');
+    artigo.classList.add('artigo');
+    main.appendChild(artigo);
+
+    // header
+    const headerArtigo = document.createElement('div');;
+    headerArtigo.classList.add('headerArtigo');
+
+    // div data
+    const data = document.createElement('div');
+            
+    // data
+    const p = document.createElement('p');
+    p.innerText = date;
+        
+    data.appendChild(p);
+
+    // div botões
+    const buttons = document.createElement('div');
+
+    const heart = document.createElement('i');
+    if(like === false) {
+        heart.classList.add('ri-heart-line', 'ri-2x', 'heart');
+    } else {
+        heart.classList.add('ri-heart-fill', 'ri-2x', 'heart');
+    }
+        
+    // add botões
+    buttons.appendChild(heart);
+
+    // adicionar no header
+    headerArtigo.appendChild(data);
+    headerArtigo.appendChild(buttons);
+
+    // conteúdo
+    const contentArticle = document.createElement('div');
+    contentArticle.classList.add('content');
+
+    // título
+    const h1 = document.createElement('h1');
+    h1.innerText = title;
+
+    // parágrafo
+    const paragraph = document.createElement('p');
+    paragraph.innerText = content;
+
+    // adicionar ao conteúdo
+    contentArticle.appendChild(h1);
+    contentArticle.appendChild(paragraph);
+
+    // adicionar ao artigo
+    artigo.appendChild(headerArtigo);
+    artigo.appendChild(contentArticle);
+
+    // evento favoritar
+    heart.addEventListener('click', () => {
+        let classe = heart.classList.contains('ri-heart-line');
+        if(classe === true) {
+            heart.classList.remove('ri-heart-line', 'ri-2x');
+            heart.classList.add('ri-heart-fill', 'ri-2x');           
+        } else {
+            heart.classList.remove('ri-heart-fill', 'ri-2x');
+            heart.classList.add('ri-heart-line', 'ri-2x');
+        };
+    });
+}
+
+window.onload = () => {
+    let post = JSON.parse(localStorage.getItem('post')) || [];
+
+    post.forEach(post => {
+        gerarPost(post.dataAtual, post.titulo, post.paragrafo, post.like);
+    });    
+};
+
+// SEARCH BAR
+let search = document.querySelector('#search');
+let post = JSON.parse(localStorage.getItem('post')) || [];
+
+search.addEventListener("input", () => {
+    let valor = search.value;
+
+    post.forEach(post => {
+        let title = post.titulo;
+
+        const regex = new RegExp(valor);
+        let result = regex.test(title);
+
+        if(result === true) {
+            let letra = regex.exec(title);
+            let searchPost = (titulo) => {
+                // selecionando o main
+                const main = document.querySelector('main');
+                const artigo = document.querySelectorAll('.artigo');
+                for(let i = 0; i < artigo.length; i++) {
+                    main.removeChild(artigo[i]);
+                };
+                
+                // gerando post
+                gerarPost(post.dataAtual, titulo, post.paragrafo, false);
+            }
+                      
+            if(valor !== '') {
+                searchPost(letra.input);
+            } else {
+                window.location.reload();
+            };
+        };
+    });
+});
